6c6b517244da6a391b7446f97a514c7bb0b3e0f7
nathanimator <njglemb@gmail.com>
2016-10-29 13:48:11 -0500

    Initial commit

---
+++ DERP_Jam/Source/DERP_Jam/DERP_Jam.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DERP_Jam.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DERP_Jam, "DERP_Jam" );
---
+++ DERP_Jam/Source/DERP_Jam/DERP_JamGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "DERP_JamGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DERP_JAM_API ADERP_JamGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
